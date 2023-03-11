f43c1afeb03ff52fa2ab593ba40f213100d4ba0e
P.Bhavana Reddy <83540871+pbhavanar@users.noreply.github.com>
2023-03-11 20:53:37 +0530

    Add files via upload

---
+++ src/Components/Auth/GsignIn.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { auth,provider } from "../../firebase";
+import {signInWithPopup} from "firebase/auth";
+
+
+function SignIn(){
+    const [value,setValue] = useState('')
+    const handleClick =()=>{
+        signInWithPopup(auth,provider).then((data)=>{
+            setValue(data.user.email)
+            localStorage.setItem("email",data.user.email)
+        })
+    }
+
+    useEffect(()=>{
+        setValue(localStorage.getItem('email'))
+    })
+
+return (
+    <div>
+        {value?<Home/>:
+        <button onClick={handleClick}>Signin With Google</button>
+        }
+    </div>
+);
+}
+export default SignIn;
